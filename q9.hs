1a74540c09183bdad2d87ce88bce199b16b20805
petitviolet <violethero0820@gmail.com>
2015-09-22 15:37:22 +0900

    q9

---
+++ q9.hs
@@ -0,0 +1,13 @@
+isTriplet :: Int -> Int -> Int -> Bool
+isTriplet a b c = sq a + sq b == sq c where
+  sq n = n * n
+
+tripletSumTo :: Int -> [[Int]]
+tripletSumTo n = [[x, y, z] |
+                 x <- [1..n `div` 3],
+                 y <- [x..n `div` 2],
+                 z <- [y..n `div` 2],
+                 isTriplet x y z && x + y + z == n]
+
+answerQ9 :: Int
+answerQ9 = product $ tripletSumTo 1000 !! 0
